e0f5b89ccd781a3261d696298439460bb2e3396e
Dagim <dagimgetaW27@gmail.com>
2024-05-07 12:12:29 +0300

    custome tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
+      },
+    },
   },
   plugins: [],
 };
