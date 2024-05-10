5bef80d0afb142cfed125ea8e3d6ae716746fcda
Dagim <dagimgetaW27@gmail.com>
2024-05-10 12:37:05 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,34 @@
+import { NavLink } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 
 export default function Navbar() {
+  const activePage = ({ isActive }) =>
+    isActive
+      ? "text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+      : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
+
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            <a
-              className="flex flex-shrink-0 items-center mr-4"
-              href="/index.html"
-            >
+            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
               <img className="h-10 w-auto" src={logo} alt="React Jobs" />
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 React Jobs
               </span>
-            </a>
+            </NavLink>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <a
-                  href="/index.html"
-                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
+                <NavLink to="/" className={activePage}>
                   Home
-                </a>
-                <a
-                  href="/jobs.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
+                </NavLink>
+                <NavLink to="/jobs" className={activePage}>
                   Jobs
-                </a>
-                <a
-                  href="/add-job.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                >
+                </NavLink>
+                <NavLink to="/add-job" className={activePage}>
                   Add Job
-                </a>
+                </NavLink>
               </div>
             </div>
           </div>
