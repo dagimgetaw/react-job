5dda294022b8cac6cca4d0557b730e21eaf32b33
Dagim <dagimgetaW27@gmail.com>
2024-05-10 12:36:19 +0300

    job listings

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { FaMapMarker } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+/* eslint-disable react/prop-types */
 
 export default function JobListing({ job }) {
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -38,12 +41,12 @@ export default function JobListing({ job }) {
             <FaMapMarker className="inline text-lg mb-1 mr-1" />
             {job.location}
           </div>
-          <a
-            href={`/job/${job.id}`}
+          <Link
+            to={`/job/${job.id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
-          </a>
+          </Link>
         </div>
       </div>
     </div>
