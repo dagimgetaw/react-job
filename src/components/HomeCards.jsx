3b9fdecd10a19196659eac70c56bfdc770f6c284
Dagim <dagimgetaW27@gmail.com>
2024-05-10 12:35:18 +0300

    homecards

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Cards from "./Cards";
 
 export default function HomeCards() {
@@ -10,24 +11,24 @@ export default function HomeCards() {
             <p className="mt-2 mb-4">
               Browse our React jobs and start your career today
             </p>
-            <a
-              href="/jobs.html"
+            <Link
+              to="/jobs"
               className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
             >
               Browse Jobs
-            </a>
+            </Link>
           </Cards>
           <Cards bg="bg-indigo-100">
             <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
             </p>
-            <a
-              href="/add-job.html"
+            <Link
+              to="/add-job"
               className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
             >
               Add Job
-            </a>
+            </Link>
           </Cards>
         </div>
       </div>
